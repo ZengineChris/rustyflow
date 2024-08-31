77005f4d1fbf2d06756566163272db873afcd355
christian <christian@zengine.dev>
2024-09-01 00:27:01 +0200

    wip: prototyp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use clap::Parser;
+
+#[derive(Parser)]
+struct CLI {
+    namespace: String,
+    command: String,
+}
+
+struct Project {
+    name: String,
+}
+
+fn main() -> Result<()> {
+    let args = CLI::parse();
+
+    // read the config file
+    let _config = std::fs::read_to_string("./.rustyflow.toml")
+        .with_context(|| format!("could not read config file"))?;
+    // check dependencies
+
+    println!(
+        "namespace: {:?}, command: {:?}",
+        args.namespace, args.command
+    );
+    Ok(())
 }
